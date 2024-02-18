9f96680d07b31d98b51ee0afd3b1127ec7d380f3
Rich Lafferty <rich@lafferty.ca>
2024-02-18 17:59:52 -0400

    Click zoom mute icon to mute/unmute

--- home/.hammerspoon/ZoomMuteState.lua
+++ home/.hammerspoon/ZoomMuteState.lua
@@ -2,24 +2,36 @@ print "Loaded ZoomMuteState.lua"
 
 -- check zoom menus to see if muted
 function updateZoomMuteState()
-  local apps = hs.application.applicationsForBundleID("us.zoom.xos")
-
-  for k, app in pairs(apps) do
+  for k, app in pairs(zoomApps()) do
     if app:findMenuItem({"Meeting", "Unmute Audio"}) then
       -- muted
-      zoomMuteState:returnToMenuBar()
-      zoomMuteState:setIcon("~/.hammerspoon/ZoomMuteState/muted.png", false)
+      zoomUpdateMenubar("~/.hammerspoon/ZoomMuteState/muted.png")
       return
     elseif app:findMenuItem({"Meeting", "Mute Audio"}) then
       -- unmuted
-      zoomMuteState:returnToMenuBar()
-      zoomMuteState:setIcon("~/.hammerspoon/ZoomMuteState/unmuted.png", false)
+      zoomUpdateMenubar("~/.hammerspoon/ZoomMuteState/unmuted.png")
       return
     end
 
     -- not in a meeting
     zoomMuteState:removeFromMenuBar()
   end
+end
+
+function zoomToggleMute()
+  for k, app in pairs(zoomApps()) do
+    hs.eventtap.keyStroke({}, "f6", 200000, app)
+    updateZoomMuteState()
+  end
+end
+
+function zoomUpdateMenubar(path)
+  -- have to set callback every time because removeFromMenubar clears it
+  zoomMuteState:returnToMenuBar():setClickCallback(zoomToggleMute):setIcon(path, false)
+end
+
+function zoomApps()
+  return hs.application.applicationsForBundleID("us.zoom.xos")
 end
 
 zoomMuteState = hs.menubar.new(false, "zoomMuteState")
